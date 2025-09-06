7922e26a92ce878a7577ca8f97156eeb04a00b82
Maryam Nasir <maryamnasir555@yahoo.com>
2025-09-06 18:22:42 +0500

    postgresql set instead of pg driver

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,9 @@ import type { Config } from 'drizzle-kit';
 export default {
   schema: './lib/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  dialect: 'postgresql',
+  // driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
