9139ba4b641ff025d53b721c37f1dbf3a409e5d0
Maryam Nasir <maryamnasir555@yahoo.com>
2025-09-09 10:51:16 +0500

    Remove drizzle sql files

--- drizzle/0000_aberrant_magdalene.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "products" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"description" text,
-	"price" numeric(10, 2) NOT NULL,
-	"image_url" varchar(512),
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL
-);
