ac3184a0feef7fe1a2f80628ae985272782a1ba7
Maryam Nasir <maryamnasir555@yahoo.com>
2025-09-09 10:27:30 +0500

    Setup database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env.local',
+});
 
 export default {
-  schema: './lib/schema.ts',
+  schema: './lib/db/schema/index.ts',
   out: './drizzle',
   dialect: 'postgresql',
-  // driver: 'pg',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
--- lib/db.ts
+++
@@ -1,6 +0,0 @@
-import { drizzle } from 'drizzle-orm/neon-serverless';
-import { Pool } from 'pg';
-
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-export const db = drizzle(pool);
