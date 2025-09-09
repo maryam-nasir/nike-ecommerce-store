574a32f8017a62ad37a1b7ac1536ca08d8ac1b2e
Maryam Nasir <maryamnasir555@yahoo.com>
2025-09-09 10:52:22 +0500

    Setup Better Auth

---
+++ lib/auth/index.ts
@@ -0,0 +1,49 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/lib/db";
+import * as schema from "@/lib/db/schema/index";
+import { v4 as uuidv4 } from "uuid";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema: {
+            user: schema.users,
+            account: schema.accounts,
+            session: schema.sessions,
+            verification: schema.verifications,
+        }
+      }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false,
+  },
+  socialProviders: {},
+  advanced: {
+    database: {
+        generateId: () => uuidv4(),
+    }
+  },
+  session: {
+    cookieCache: {
+        enabled: true,
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+    },
+    
+  },
+  cookies: {
+    sessionToken: {
+        name: "auth_session",
+        options: {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            path: "/",
+            maxAge: 60 * 60 * 24 * 7, // 7 days
+        }
+    }
+  },
+  plugins: [nextCookies()]
+ 
+});
